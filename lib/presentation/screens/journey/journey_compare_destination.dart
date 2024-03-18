d6cd9039cf69f3998439c16ea1058e2d9bcd8b10
davidakinfenwa <davidkayode50@gmail.com>
2024-03-18 10:59:47 +0100

    Hello new

--- lib/presentation/screens/journey/journey_compare_destination.dart
+++ lib/presentation/screens/journey/journey_compare_destination.dart
@@ -123,7 +123,7 @@ class _JoureyCompareDestinationPageState
               ),
             ],
           ),
-          SizedBox(width: (Sizing.kSizingMultiple*4).h),
+          SizedBox(width: (Sizing.kSizingMultiple * 4).h),
           Column(
             children: [
               SizedBox(
@@ -143,7 +143,7 @@ class _JoureyCompareDestinationPageState
               ),
             ],
           ),
-         SizedBox(width: (Sizing.kSizingMultiple*4).h),
+          SizedBox(width: (Sizing.kSizingMultiple * 4).h),
           Column(
             children: [
               SizedBox(
@@ -163,169 +163,169 @@ class _JoureyCompareDestinationPageState
               ),
             ],
           ),
-         SizedBox(width: (Sizing.kSizingMultiple).h),
-
+          SizedBox(width: (Sizing.kSizingMultiple).h),
         ],
       ),
     );
   }
 
   Widget _buildWhatEverToggle() {
-    List<Map<String,dynamic>> items = 
-[
-    {
-      "title": "Housing Costs",
-      "item_list": [
-        {
-          "item_list_name": "apartment rent",
-          "uk": {"cost": 200000},
-          "canada": {"cost": 200000},
-          "us": {"cost": 200000}
-        },
-        {
-          "item_list_name": "Median home price (3BR, 2BA)",
-          "uk": {"cost": 200000},
-          "canada": {"cost": 200000},
-          "us": {"cost": 200000}
-        }
-      ]
-    },
-    {
-      "title": "Transportation Costs",
-      "item_list": [
-        {
-          "item_list_name": "Limoxing",
-          "uk": {"cost": 200000},
-          "canada": {"cost": 200000},
-          "us": {"cost": 200000}
-        },
-        {
-          "item_list_name": "ferrari",
-          "uk": {"cost": 200000},
-          "canada": {"cost": 200000},
-          "us": {"cost": 200000}
-        }
-      ]
-    }
-  ];
+    List<Map<String, dynamic>> items = [
+      {
+        "title": "Housing Costs",
+        "item_list": [
+          {
+            "item_list_name": "apartment rent",
+            "uk": {"cost": 200000},
+            "canada": {"cost": 200000},
+            "us": {"cost": 200000}
+          },
+          {
+            "item_list_name": "Median home price (3BR, 2BA)",
+            "uk": {"cost": 200000},
+            "canada": {"cost": 200000},
+            "us": {"cost": 200000}
+          }
+        ]
+      },
+      {
+        "title": "Transportation Costs",
+        "item_list": [
+          {
+            "item_list_name": "Limoxing",
+            "uk": {"cost": 200000},
+            "canada": {"cost": 200000},
+            "us": {"cost": 200000}
+          },
+          {
+            "item_list_name": "ferrari",
+            "uk": {"cost": 200000},
+            "canada": {"cost": 200000},
+            "us": {"cost": 200000}
+          }
+        ]
+      }
+    ];
 
     return ListView.builder(
-      padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-      itemCount: items.length,
-      shrinkWrap: true,
-      itemBuilder: (context,index){
-        final data=items[index];
-return  Padding(
-  padding: const EdgeInsets.only(bottom: 20),
-  child: Column(
-    
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            width: MediaQuery.sizeOf(context).width,
-            padding: const EdgeInsets.symmetric(horizontal: 18.57, vertical: 8),
-decoration: BoxDecoration(
-color: Color(0xFFF2F2F2),
-borderRadius: BorderRadius.only(
-topLeft: Radius.circular(8),
-topRight: Radius.circular(8),
-),
-border: Border(
-left: BorderSide(color: Color(0xFFEAECF0)),
-top: BorderSide(color: Color(0xFFEAECF0)),
-right: BorderSide(color: Color(0xFFEAECF0)),
-bottom: BorderSide(width: 1.16, color: Color(0xFFEAECF0)),
-),
-),
-            child: Text(
-              data['title'],
-              style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                    fontWeight: FontWeight.w500,
-                    color: Color(0xFF191D23),
+        padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+        itemCount: items.length,
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          final data = items[index];
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: MediaQuery.sizeOf(context).width,
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 18.57, vertical: 8),
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFF2F2F2),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(8),
+                      topRight: Radius.circular(8),
+                    ),
+                    border: Border(
+                      left: BorderSide(color: Color(0xFFEAECF0)),
+                      top: BorderSide(color: Color(0xFFEAECF0)),
+                      right: BorderSide(color: Color(0xFFEAECF0)),
+                      bottom: BorderSide(width: 1.16, color: Color(0xFFEAECF0)),
+                    ),
                   ),
-            ),
-          ),
-            SizedBox(height: (Sizing.kSizingMultiple).h),
-            
-  
-     Column(children: List.generate(data['item_list'].length, (index) {
-      final result= data['item_list'][index];
-      return      Container(
-        padding: const EdgeInsets.symmetric(horizontal: 18.57, vertical: 8),
-       decoration: BoxDecoration(
-
-border: Border(
+                  child: Text(
+                    data['title'],
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF191D23),
+                        ),
+                  ),
+                ),
+                SizedBox(height: (Sizing.kSizingMultiple).h),
+                Column(
+                  children: List.generate(data['item_list'].length, (index) {
+                    final result = data['item_list'][index];
+                    return Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 18.57, vertical: 8),
+                      decoration: BoxDecoration(
+                        border: Border(
 // left: BorderSide(color: Color(0xFFEAECF0)),
 // top: BorderSide(color: Color(0xFFEAECF0)),
 // right: BorderSide(color: Color(0xFFEAECF0)),
-bottom: BorderSide(width: 1.16, color: Color(0xFFEAECF0)),
-),
-),
-        child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                 SizedBox(
-                    width: 120,
-                    //height: 120,
-                    child: Text(
-                      result['item_list_name'],
-                      style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                            fontWeight: FontWeight.w400,
-                            color: Color(0xFF191D23),
+                          bottom:
+                              BorderSide(width: 1.16, color: Color(0xFFEAECF0)),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(
+                            width: 120,
+                            //height: 120,
+                            child: Text(
+                              result['item_list_name'],
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall
+                                  ?.copyWith(
+                                    fontWeight: FontWeight.w400,
+                                    color: Color(0xFF191D23),
+                                  ),
+                            ),
                           ),
-                    ),
-                  ),
-          
-                    SizedBox(
-                    width: 50,
-                   // height: 32,
-                    child: Text(
-                      "\$3,709",
-                      style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                            fontWeight: FontWeight.w400,
-                            color: Color(0xFF191D23),
+                          SizedBox(
+                            width: 50,
+                            // height: 32,
+                            child: Text(
+                              "\$3,709",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall
+                                  ?.copyWith(
+                                    fontWeight: FontWeight.w400,
+                                    color: Color(0xFF191D23),
+                                  ),
+                            ),
                           ),
-                    ),
-                  ),
-          
-                    SizedBox(
-                    width: 50,
-                    
-                    child: Text(
-                      "\$3,709",
-                      style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                            fontWeight: FontWeight.w400,
-                            color: Color(0xFF191D23),
+                          SizedBox(
+                            width: 50,
+                            child: Text(
+                              "\$3,709",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall
+                                  ?.copyWith(
+                                    fontWeight: FontWeight.w400,
+                                    color: Color(0xFF191D23),
+                                  ),
+                            ),
                           ),
-                    ),
-                  ),
-          
-                    SizedBox(
-                    width: 50,
-                   
-                    child: Text(
-                      "\$3,709",
-                      style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                            fontWeight: FontWeight.w400,
-                            color: Color(0xFF191D23),
+                          SizedBox(
+                            width: 50,
+                            child: Text(
+                              "\$3,709",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall
+                                  ?.copyWith(
+                                    fontWeight: FontWeight.w400,
+                                    color: Color(0xFF191D23),
+                                  ),
+                            ),
                           ),
-                    ),
-                  ),
-                
+                        ],
+                      ),
+                    );
+                  }),
+                ),
               ],
             ),
-      );
-     })
-     ,),
-       
-        ],
-      ),
-);
-    });
-    
-    
-    
+          );
+        });
   }
 
   Widget _buildActionButton() {
